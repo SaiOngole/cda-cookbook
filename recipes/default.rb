4366831e45e9e3fb50eefe8c318a71080e51937c
Sai Ongole <svo214@nyu.edu>
2015-05-15 15:13:03 -0400

    Grails symbolic link

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,7 @@ bash "update apt" do
 	code "sudo apt-get update"
 end
 
-package ["curl","vim","git","git-flow","memcached"] do 
+package ["curl","vim","git","git-flow","memcached","unzip"] do 
 	action :install
 	
 end
@@ -22,7 +22,22 @@ bash "hosts_entry" do
 end
 
 remote_file "/tmp/grails.zip" do
-	
 	source "https://github.com/grails/grails-core/releases/download/v2.4.4/grails-2.4.4.zip"
 	mode '0644'
+end
+
+directory "Applications" do
+	owner "root"
+	group "root"
+	mode '0777'
+	action :create
+end
+
+bash "move_grails" do
+	cwd "/tmp"
+	code <<-EOH
+	unzip grails.zip
+	mv /tmp/grails-2.4.4 /Applications
+	ln -s /Applications/grails-2.4.4/bin/grails /usr/bin/grails
+	EOH
 end
